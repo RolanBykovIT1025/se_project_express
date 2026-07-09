2430eee48094ef85c0772f2b61b155ad6f6275bf
Rolan Bykov <Rbykov30@gmail.com>
2026-07-08 22:45:23 -0400

    Add dotenv and JWT_SECRET environment variable config

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,5 @@
-const config = {
-  JWT_SECRET:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp - QV30;",
-};
+const { JWT_SECRET = "super-strong-secret" } = process.env;
 
-module.exports = config;
+module.exports = {
+  JWT_SECRET,
+};
