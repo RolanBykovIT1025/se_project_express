217616bef2b0f96347973036e122b63bc0437ad3
Rolan Bykov <Rbykov30@gmail.com>
2026-07-10 17:27:16 -0400

    refactor: split error classes into individual files under utils/errors/ per linter

---
+++ utils/errors/index.js
@@ -0,0 +1,21 @@
+const BadRequestError = require("./BadRequestError");
+const UnauthorizedError = require("./UnauthorizedError");
+const ForbiddenError = require("./ForbiddenError");
+const NotFoundError = require("./NotFoundError");
+const ConflictError = require("./ConflictError");
+
+module.exports = {
+  BadRequestError,
+  UnauthorizedError,
+  ForbiddenError,
+  NotFoundError,
+  ConflictError,
+
+  // Status code constants
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  CONFLICT: 409,
+  SERVER_ERROR: 500,
+};
